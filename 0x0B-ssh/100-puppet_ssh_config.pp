e3ca7dfabdf5ef568f41b2e7a15d3643e8a21956
rallmac2 <root@tobijahdevice>
2024-06-29 15:05:18 +0100

    task 100

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# make changes to config file using puppet
+file {'/etc/ssh/ssh_config':
+  ensure  => 'present',
+}
+file_line {'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  match   => 'PasswordAuthentication yes',
+  replace => 'true',
+}
+file_line {'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+  match  => '^IdentityFile',
+}
