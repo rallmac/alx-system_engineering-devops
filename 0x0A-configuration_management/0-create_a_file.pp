52d64e4259e497d04974312f1a02cc59eaa40675
rallmac2 <root@tobijahdevice>
2024-06-29 05:38:44 +0100

    task 0 redo

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,10 @@
-file { '/tmp/school':
-  ensure  => 'file',
-  content => 'I love Puppet',
+#Creating a file using puppet in /tmp
+
+file { 'the_path':
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
+  content => 'I love Puppet',
 }
+
