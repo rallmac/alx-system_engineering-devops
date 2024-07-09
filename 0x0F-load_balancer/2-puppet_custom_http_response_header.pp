3598215abdfb06de970029bd01683ca1c3e90092
rallmac2 <root@tobijahdevice>
2024-07-09 14:50:05 +0100

    task 1 and 2 redo

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,12 +1,30 @@
-http_response_header.pp
+# Setup New Ubuntu server with nginx
+# and add a custom HTTP header
 
-#!/usr/bin/env bash
-# configure with puppet
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
+}
 
-exec { 'http header':
-  command => 'sudo apt-get update -y &&
-              sudo apt-get install nginx -y &&
-              sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default &&
-              sudo service nginx restart',
-  provider => shell,
+package { 'nginx':
+	ensure => 'installed',
+	require => Exec['update system']
+}
+
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
+}
+
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+exec {'HTTP header':
+	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
 }
