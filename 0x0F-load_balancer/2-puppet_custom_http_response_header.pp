d773160c8ed5d8694a65b2d09df2cfb5ba64e587
rallmac2 <root@tobijahdevice>
2024-07-09 05:55:36 +0100

    task 1 and 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,12 @@
+http_response_header.pp
+
+#!/usr/bin/env bash
+# configure with puppet
+
+exec { 'http header':
+  command => 'sudo apt-get update -y &&
+              sudo apt-get install nginx -y &&
+              sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default &&
+              sudo service nginx restart',
+  provider => shell,
+}
