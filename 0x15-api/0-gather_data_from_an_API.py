29f508bcbcf2caf7a2862c43c94596da3d8b479a
rallmac2 <root@tobijahdevice>
2024-07-30 02:12:54 +0100

    task 0 redo

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -19,7 +19,8 @@ if __name__ == "__main__":
         if todo.get("completed") is True:
             completed.append(todo.get("title"))
 
-    print("Employee {} is done with tasks({}/{}):".format(user.get("name"), len(completed), len(todos)))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user.get("name"), len(completed), len(todos)))
 
     for complete in completed:
-        print("\t {}".format(completed))
+        print("\t {}".format(complete))
