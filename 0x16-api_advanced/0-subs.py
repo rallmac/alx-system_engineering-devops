66744c227edd734a3815f9cca4c79050d3cbb33d
rallmac2 <root@tobijahdevice>
2024-08-15 20:42:33 +0100

    task 0 again

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,15 +10,14 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the number of subscribers for a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'custom-user-agent'}
+    headers = {'User-Agent': 'my-custom-user-agent'}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
-            return data['data']['subscribers']
+            return data.get('data', {}).get('subscribers', 0)
         else:
             return 0
     except requests.RequestException:
         return 0
-
